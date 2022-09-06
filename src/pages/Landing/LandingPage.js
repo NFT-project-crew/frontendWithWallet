b39da1ceaf090082d1f2097baaccc940f431c335
Filip Strelec <filip.strelec@gmail.com>
2022-09-06 20:40:29 +0200

    Fix router

--- src/pages/Landing/LandingPage.js
+++ src/pages/Landing/LandingPage.js
@@ -1,5 +1,4 @@
 
-import * as React from 'react';
 // import  {Link} from "react-router-dom";
 import {Page, PageDocumentHead, PageHeader} from "../Components/index";
 // import styled from 'styled-components';
@@ -23,7 +22,7 @@ return (
     
 testiram  
  <MainSection>
-fwdfaw
+    ss
  </MainSection>
 
 
---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Testiram = ()=>{
+    const REACT_VERSION = React.version;
+    return(<>React version: {REACT_VERSION}</>)
+  
+  }
+
+  export default Testiram
